aa75bb9ee78a33f42bb68129bfa83761f96fba2e
Nadif-Azeddine <112632224+Nadif-Azeddine@users.noreply.github.com>
2023-11-21 11:29:13 +0100

    Delete src/main/java/com/cours/spring2/repo directory

--- src/main/java/com/cours/spring2/repo/NoteRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cours.spring2.repo;
-
-import com.cours.spring2.model.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepo extends JpaRepository<Note, Integer> {
-}
